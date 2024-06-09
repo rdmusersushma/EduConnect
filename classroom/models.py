bbb5bd927266b9e635e89fdd82d6893c77a1ddff
rdmusersushma <marathisushma@gmail.com>
2024-06-09 12:11:56 +0530

    My first commit

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=100, blank=True)
+    is_student = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+# Create your models here.
+
+
+
--- classroom/views.py
+++ classroom/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django import forms
+from formtools.wizard.views import SessionWizardView
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
 
 def home(request):
@@ -7,3 +11,16 @@ def home(request):
 def about(request):
 	return render(request, 'classroom/about.html')
 # Create your views here.
+
+
+class UserCreationWizard(SessionWizardView):
+    def done(self, form_list, form_dict, **kwargs):
+        user = form_list[0]
+        student  = form_list[1]
+        if user.is_valid() and student.is_valid():
+            user.save()
+            student.username = user.cleaned_data.get('username')
+            student.save()
+        else:
+            print("either user or student is not valid")
+        return HttpResponseRedirect('/slogin')
